e0b29fe80aacfd947d9a20f8cf364e266058f4b0
Naif Amoodi <amo21004@byui.edu>
2022-10-14 16:43:25 +0530

    14/Oct/2022 - Second commit

--- controllers/songs.js
+++ controllers/songs.js
@@ -22,7 +22,7 @@ module.exports = (dependencies) => {
             #swagger.description = 'Get a specific song by song_id'
         */
 
-        response.status(200).send('You are at /songs/:song_id');
+        response.status(200).send('You are at /songs/:song_id (GET)');
     });
 
     // The second callback over here makes authentication required for this endpoint
@@ -34,7 +34,7 @@ module.exports = (dependencies) => {
             #swagger.parameters['obj'] = {
                 in: 'body',
                 '@schema': {
-                    "required": ["title"],
+                    "required": ["title", "release_year", "summary"],
                     "properties": {
                         "title": {
                             "type": "string",
@@ -67,6 +67,71 @@ module.exports = (dependencies) => {
         */
 
         response.status(200).send('You are at /songs (POST)');
+    });
+
+    router.put('/:song_id', dependencies.requires_authentication(), (request, response, next) => {
+        /*
+            #swagger.parameters['song_id'] = {
+                in: 'query',
+                description: 'The ID of the song to update',
+                required: true,
+                type: 'number'
+            }
+            #swagger.path = '/songs'
+            #swagger.tags = ['Song']
+            #swagger.description = 'Updates a song specified by song_id'
+            #swagger.parameters['obj'] = {
+                in: 'body',
+                '@schema': {
+                    "required": ["title", "release_year", "summary"],
+                    "properties": {
+                        "title": {
+                            "type": "string",
+                            "example": "Some title..."
+                        },
+                        "release_year": {
+                            "type": "string",
+                            "example": "Some release year..."
+                        },
+                        "rating": {
+                            "type": "number",
+                            "example": "Some rating..."
+                        },
+                        "summary": {
+                            "type": "string",
+                            "example": "Some summary..."
+                        },
+                        "artists": {
+                            "type": []
+                        },
+                        "genres": {
+                            "type": []
+                        },
+                        "reviews": {
+                            "type": []
+                        }
+                    } 
+                }
+            }
+        */
+
+        response.status(200).send('You are at /:song_id (PUT)');
+    });
+
+    router.delete('/:song_id', dependencies.requires_authentication(), (request, response, next) => {
+        /*
+            #swagger.parameters['song_id'] = {
+                in: 'query',
+                description: 'The ID of the song to delete',
+                required: true,
+                type: 'number'
+            }
+            #swagger.path = '/songs/:song_id'
+            #swagger.tags = ['Song']
+            #swagger.description = 'Deletes a song specified by song_id'
+        */
+
+        response.status(200).send('You are at /songs/:song_id (DELETE)');
     });
 
     return router;
