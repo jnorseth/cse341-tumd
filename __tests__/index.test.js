831597cb8a4fb945b19d5837657b8ba785b73ba7
Naif Amoodi <amo21004@byui.edu>
2022-10-21 23:51:51 +0530

    Post-merge 1

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -15,3 +15,11 @@ test('check single user api endpoint (/users/6347102ab16a3ca89f638858)', async (
         `{"id":"6347102ab16a3ca89f638858","nickname":"amo21004","picture":"amo21004","role":"Administrator"}`
     );
 });
+
+test('check single review api endpoint (/reviews/63486dab644380993a785107)', async () => {
+    expect(JSON.stringify
+        ((await axios('http://localhost:3000/reviews/63486dab644380993a785107')).data)
+    ).toBe(
+        `{"_id":"63486dab644380993a785107","rating":5,"user":"amo21004","body":"Great song!","song":"What a Wonderful World"}`
+    );
+});
